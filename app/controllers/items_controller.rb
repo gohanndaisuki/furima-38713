3861f53f73e2a3aaea75a63bfc5b4fbe5b9f6d68
gohanndaisuki <117286732+gohanndaisuki@users.noreply.github.com>
2022-12-04 12:06:48 +0900

    ユーザー管理機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
-    
   end
 
+  def new
+  end
+
+  def destroy
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+  validates :nickname, :birth_date,  presence: true
+  validates :last_name,              presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+  validates :last_name_kana,         presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
+  validates :first_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+  validates :first_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
+end
