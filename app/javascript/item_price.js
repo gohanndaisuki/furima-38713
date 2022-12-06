319a6e2fb82a4e521d9bab7943707879b7e5710a
gohanndaisuki <117286732+gohanndaisuki@users.noreply.github.com>
2022-12-06 14:15:25 +0900

    ユーザー編集機能２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,8 +2,8 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
    priceInput.addEventListener("input", () => {
      const saleTax = document.getElementById("add-tax-price");
-     saleTax.innerHTML = Math.round(priceInput.value * 0.1 );
+     saleTax.innerHTML = Math.floor(priceInput.value * 0.1 );
      const salePofit = document.getElementById("profit");
-     salePofit.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+     salePofit.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 });
